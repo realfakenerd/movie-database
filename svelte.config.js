502302a52e64ae81fd86f0cdf5af4447f8ed41aa
realfakenerd <lucas210898@hotmail.com>
2022-04-06 19:21:22 -0300

    added better compression

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,29 @@
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
-
+import viteCompression from 'vite-plugin-compression';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
 	preprocess: preprocess(),
-
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		vite: {
+			build: {
+				reportCompressedSize: false,
+				target: "esnext",
+				minify: "terser",
+			},
+			plugins: [
+				viteCompression({
+					algorithm: "brotliCompress",
+					threshold: 512,
+					compressionOptions: {
+						level: 3,
+					},
+				})
+			]
+		}
 	}
 };
 
