b221d0d7860beaada3a8dec2bd517ca34b6007c6
realfakenerd <lucas210898@hotmail.com>
2022-05-26 01:56:09 -0300

    made some changes to the configurations

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,26 @@
 module.exports = {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
+	content: [
+		'./src/**/*.{html,js,svelte,ts}',
+	],
 	theme: {
-		extend: {}
+		
+		extend: {
+			colors: {
+				'primary': '#e44e46',
+				'secondary': '#418eff',
+				'accent': '#c6a727',
+				'neutral': "#191D24",
+				"neutral-focus": "#111318",
+				"neutral-content": "#A6ADBB",
+				'base-100': '#1d252e',
+				"base-200": "#242933",
+				"base-300": "#20252E",
+				"base-content": "#A6ADBB"
+			},
+		}
 	},
 	mode: 'jit',
-	plugins: [require('daisyui'), require('@tailwindcss/forms'), require('@tailwindcss/typography')],
+	plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
 	daisyui: {
 		themes: [
 			{
