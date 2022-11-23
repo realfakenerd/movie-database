e76c758393afc437cafdd62e8f1faab2624b9c85
realfakenerd <lucas210898@hotmail.com>
2022-11-23 17:26:54 -0300

    another change

--- src/routes/[type]/[id]/+page.svelte
+++ src/routes/[type]/[id]/+page.svelte
@@ -47,20 +47,18 @@
 						<div class="stat-desc">Movie status: <span class="badge">{movie.status}</span></div>
 					</div>
 
-					<div class="stat place-items-center">
+					<div class="stat">
 						<div class="stat-figure text-primary">
 							<svg
 								xmlns="http://www.w3.org/2000/svg"
-								fill="none"
-								viewBox="0 0 24 24"
-								class="inline-block w-8 h-8 stroke-current"
-								><path
-									stroke-linecap="round"
-									stroke-linejoin="round"
-									stroke-width="2"
-									d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-								/></svg
+								class="inline-block w-8 h-8  text-yellow-300"
+								viewBox="0 0 20 20"
+								fill="currentColor"
 							>
+								<path
+									d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+								/>
+							</svg>
 						</div>
 						<div class="stat-title">Movie score</div>
 						<div class="stat-value text-yellow-300">{movie.vote_average}</div>
