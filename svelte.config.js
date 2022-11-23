6f3f67c64fe6efc99dd2b873b96174d838c0323c
realfakenerd <lucas210898@hotmail.com>
2022-11-23 15:36:06 -0300

    small fix

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,14 @@
-import vercel from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-vercel';
 import preprocess from 'svelte-preprocess';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess(),
+	preprocess: preprocess({
+		scss: true
+	}),
 	kit: {
-		adapter: vercel(),
+		adapter: adapter(),
 		version: {
 			name: Math.ceil(Math.random() * Date.now())
 				.toString()
